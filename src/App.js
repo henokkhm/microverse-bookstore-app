044d1ac413225c23a282ccab32245cec500a3a89
Henok Kirubel <henokhm2@gmail.com>
2023-07-21 21:51:57 +0300

    Add home and categories routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Categories from './pages/Categories';
 import styles from './styles/App.module.css';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <header>Learn React</header>
-    </div>
+    <>
+      <main className={styles.app}>
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="categories" element={<Categories />} />
+          </Routes>
+        </div>
+      </main>
+    </>
   );
 }
 
