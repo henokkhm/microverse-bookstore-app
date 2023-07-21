3b8fa0e4c521c8efe65b164ff52254434f717cd9
Henok Kirubel <henokhm2@gmail.com>
2023-07-21 22:06:59 +0300

    Add CSS reset

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/reset.module.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
