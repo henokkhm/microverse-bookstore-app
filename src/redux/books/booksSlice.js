a6038fadb63b9c99563e98cdfa00eb28d4bcbe03
Henok Kirubel <henokhm2@gmail.com>
2023-07-27 10:35:01 +0300

    Add addBook reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -8,6 +8,14 @@ const initialState = {
 const booksSlice = createSlice({
   name: 'books',
   initialState,
+  reducers: {
+    addBook: (state, { payload }) => {
+      const { title, category } = payload;
+      state.books.push({ title, category });
+    },
+  },
 });
+
+export const { addBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
