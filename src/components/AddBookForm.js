24cff18fec9fcb4f9e9914201a77bcd98b457244
Henok Kirubel <henokhm2@gmail.com>
2023-07-27 19:01:14 +0300

    Change AddBook form inputs into controlled components

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import styles from '../styles/AddBookForm.module.css';
 
 function AddBookForm() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('form submitted');
+  };
+
   return (
     <section>
       <h2>Add New Book</h2>
-      <form action="#" className={styles.addBookForm}>
+      <form action="#" onSubmit={handleSubmit} className={styles.addBookForm}>
         <label className={styles.inputWrapper} htmlFor="title">
           <span className={styles.label}>Title</span>
           <input
@@ -12,24 +21,30 @@ function AddBookForm() {
             type="text"
             id="title"
             name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             placeholder="The Hobbit"
             required
           />
         </label>
 
-        <label className={styles.inputWrapper} htmlFor="category">
-          <span className={styles.label}>Category</span>
+        <label className={styles.inputWrapper} htmlFor="author">
+          <span className={styles.label}>Author</span>
           <input
             className={styles.inputs}
             type="text"
-            id="category"
-            name="category"
-            placeholder="Fantasy"
+            id="author"
+            name="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            placeholder="J.R.R. Tolkien"
             required
           />
         </label>
 
-        <button className={styles.btn} type="submit">Add</button>
+        <button className={styles.btn} type="submit">
+          Add
+        </button>
       </form>
     </section>
   );
