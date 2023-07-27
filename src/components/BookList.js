d492b41c507c0bab9b715189273db90f7666c128
Henok Kirubel <henokhm2@gmail.com>
2023-07-27 17:54:15 +0300

    Add condition to check if zero books in the booksList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,6 +5,12 @@ import Book from './Book';
 function BookList() {
   const { booksList } = useSelector((state) => state.books);
 
+  if (!booksList.length) {
+    return (
+      <div>You have no books yet! Please use the form below to add a book.</div>
+    );
+  }
+
   return (
     <div>
       <h2>My List of Books</h2>
